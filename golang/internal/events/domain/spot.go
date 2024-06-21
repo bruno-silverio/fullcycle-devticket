56d8bb78e179ae338eba8842bca08f275f9013c3
bruno-silverio <bcamilo94@gmail.com>
2024-06-21 12:23:57 -0300

    feat: add spot validate

--- golang/internal/events/domain/spot.go
+++ golang/internal/events/domain/spot.go
@@ -1,5 +1,19 @@
 package domain
 
+import (
+	"errors"
+)
+
+var (
+	ErrInvalidSpotNumber       = errors.New("invalid spot number")
+	ErrSpotNotFound            = errors.New("spot not found")
+	ErrSpotAlreadyReserved     = errors.New("spot already reserved")
+	ErrSpotNameRequired		   	 = errors.New("spot name is required")
+	ErrSpotNameTwoCharacters	 = errors.New("spot name must be at least 2 characters long")
+	ErrSpotNameStartWithLetter = errors.New("spot name must start with a letter")
+	ErrSpotNameEndWithNumber	 = errors.New("spot name must end with a number")
+)
+
 type SpotStatus string
 
 const (
@@ -13,4 +27,22 @@ type Spot struct {
 	Name 			string
 	Status 		SpotStatus
 	TicketID 	string
+}
+
+// Validate checks if the spot data is valid.
+func (s *Spot) Validate() error {
+	if len(s.Name) == 0 {
+		return ErrSpotNameRequired
+	}
+	if len(s.Name) < 2 {
+		return ErrSpotNameTwoCharacters
+	}
+	// Validate if the spot name is in the correct format
+	if s.Name[0] < 'A' || s.Name[0] > 'Z' {
+		return ErrSpotNameStartWithLetter
+	}
+	if s.Name[1] < '0' || s.Name[1] > '9' {
+		return ErrSpotNameEndWithNumber
+	}
+	return nil
 }
