3c2ccab22ee62605ce6db85f0baf632bcb882562
bruno-silverio <bcamilo94@gmail.com>
2024-06-21 14:17:09 -0300

    feat: add ticket validate and calculate price

--- golang/internal/events/domain/ticket.go
+++ golang/internal/events/domain/ticket.go
@@ -1,5 +1,9 @@
 package domain
 
+import "errors"
+
+var ErrTicketPriceZero = errors.New("ticket price must be greater than zero")
+
 type TicketType string
 
 const (
@@ -13,4 +17,24 @@ type Ticket struct {
 	Spot 				*Spot
 	TicketType 	TicketType
 	Price				float64
+}
+
+// IsValidTicketKind checks if a ticket kind is valid.
+func IsValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+}
+
+// CalculatePrice calculates the price based on the ticket kind.
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2
+	}
+}
+
+// Validate checks if the ticket data is valid.
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return ErrTicketPriceZero
+	}
+	return nil
 }
