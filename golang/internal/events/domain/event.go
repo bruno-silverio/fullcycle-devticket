e54b859c7f3dd752fc318e73d73e3d1b2a7eace8
bruno-silverio <bcamilo94@gmail.com>
2024-06-21 12:10:32 -0300

    feat: add event structure

---
+++ golang/internal/events/domain/event.go
@@ -0,0 +1,31 @@
+package domain
+
+import (
+	"time"
+)
+
+// Rating represents the age restriction for an event
+type Rating string
+
+const (
+	RatingLivre Rating = "L"
+	Rating10    Rating = "L10"
+	Rating12    Rating = "L12"
+	Rating14    Rating = "L14"
+	Rating16    Rating = "L16"
+	Rating18    Rating = "L18"
+)
+
+// Event represents an event with tickets and spots.
+type Event struct {
+	ID           string
+	Name         string
+	Location     string
+	Organization string
+	Rating       Rating
+	Date         time.Time
+	ImageURL     string
+	Capacity     int
+	Price        float64
+	PartnerID    int
+}
