0fe68876867d737daf8ceac615c4e91aecc6565a
bruno-silverio <bcamilo94@gmail.com>
2024-06-24 12:26:49 -0300

    feat: add infra/http handler - GetEvent

--- golang/internal/events/infra/http/events_handler.go
+++ golang/internal/events/infra/http/events_handler.go
@@ -55,3 +55,29 @@ func (h *EventsHandler) ListEvents(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(output)
 }
+
+// GetEvent handles the request to get details of a specific event.
+// @Summary Get event details
+// @Description Get details of an event by ID
+// @Tags Events
+// @Accept json
+// @Produce json
+// @Param eventID path string true "Event ID"
+// @Success 200 {object} usecase.GetEventOutputDTO
+// @Failure 400 {object} string
+// @Failure 404 {object} string
+// @Failure 500 {object} string
+// @Router /events/{eventID} [get]
+func (h *EventsHandler) GetEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := r.PathValue("eventID")
+	input := usecase.GetEventInputDTO{ID: eventID}
+
+	output, err := h.getEventUseCase.Execute(input)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(output)
+}
