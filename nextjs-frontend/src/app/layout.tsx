d89746cb2f208ce138c8c99b7e3aa8970ad0eaf4
bruno-silverio <bcamilo94@gmail.com>
2024-06-26 10:47:32 -0300

    feat: add event

--- nextjs-frontend/src/app/layout.tsx
+++ nextjs-frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "../components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} flex flex-col min-h-screen items-center bg-primary text-default`}
+      >
+        <div className="p-4 md:p-10 w-full max-w-[1256px]">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
